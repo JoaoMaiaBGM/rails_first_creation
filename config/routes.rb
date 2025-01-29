880fc6493aa702c82bcd57a417ec2edd6e2ae4c5
JoaoMaia <joaocarlosbgm@gmail.com>
2025-01-28 23:50:02 -0300

    feat: controller, view and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #get "/products", to: "products#index"
+  #get "/products/:id", to: "products#show"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  #get "/products/new", to "products#new"
+  #post "/products", to: "products#create"
+  
+  #get "/products/:id", to "products#edit"
+  #put "/products/:id", to "products#update"
+  #patch "/products/:id", to: "products#update"
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  #delete "/products/:id", to: "products#destroy"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # This is how tippically we'll written our routes. 
+  # But typing out these routes every time is redundant, 
+  # so Rails provides a shortcut for defining them. To create all of the same CRUD routes,
+  # replace the above routes with this single line:
+
+  resources :products
+  root "products#index"
 end
