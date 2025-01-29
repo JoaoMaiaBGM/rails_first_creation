b434f7015817ac6b7bcbf0ada0ca65615790fa40
JoaoMaia <joaocarlosbgm@gmail.com>
2025-01-29 15:39:40 -0300

    fix:user_controller test

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -8,6 +8,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should get create" do
     post users_path, params: { user: { email_address: "test@example.com", password: "password", password_confirmation: "password" } }
-    assert_response :rediretc
+    assert_response :redirect
   end
 end
